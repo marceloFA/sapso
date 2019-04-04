26ca3af8bc262fe5898a7aada93b31f4724cb308
Marcelo Almeida <jmfda00@gmail.com>
2019-04-04 10:33:44 -0300

    Improve algorithm performance
    Improve performance through algorithm  flowchart revision

--- sapso.py
+++ sapso.py
@@ -60,7 +60,7 @@ class TestFunctions():
 
 def calculate_diversity_and_dir(d_low, d_high, n, L, swarm, dir_):
   """  Calculates the diversity factor, 
-       which may be -1 (rstop_criterionulsion phase) or 1 (attraction phase)
+       which may be -1 (repislonulsion phase) or 1 (attraction phase)
   """
   mean = np.mean(swarm, axis=0)
   summ = 0
@@ -108,7 +108,7 @@ def plot_swarm(swarm):
 
 
 
-def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_max, d_low, d_high, stop_criterion, f_name):
+def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_max, d_low, d_high, epislon, f_name, stop_criterion):
   #TODO: docstring later on
   '''
     The Semi Autonomus particle swarm optmizer
@@ -141,6 +141,8 @@ def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_ma
   best_fitness_history = []                       # Best fitness history (for each iteration)
 
   objective_function = getattr(TestFunctions(),f_name)
+
+  epsilon_2 = 1e-5                                # how does the second epsilon works?
   
   # For each particle:
   for k in range(n):
@@ -157,9 +159,7 @@ def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_ma
   
   # Main loop:
   for i in range(m):
-      #Visualize swarm every 100 iterations:
-      #if (m % 100 == 0): plot_swarm(swarm)
-      #Calculate inertia as a function of remaining iterations
+      #Calculate inertia as a function of remaining iterations:
       inertia = (min_inertia - (min_inertia - max_inertia)) * (i/m); 
       
       #For each particle:
@@ -172,6 +172,7 @@ def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_ma
           if I[k] == 0:
               gradient = calculate_gradient(objective_function, swarm[k], n_dimensions)
           
+          # Calculate particle velocity:
           phi_1 = np.random.uniform(size=n_dimensions)
           phi_2 = np.random.uniform(size=n_dimensions)
           velocity = (inertia*velocities[k]) + dir_ * (I[k]*c1*phi_1*(best_global_position - swarm[k]) + (I[k]-1)*c2*phi_2*gradient)
@@ -179,13 +180,15 @@ def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_ma
           #For each dimension:
           for j in range(n_dimensions):
               #Validate velocity:
-              if velocity[j] > v_max: velocity[j] = 0
-              if velocity[j] < -v_max: velocity[j] = 0
+              if velocity[j] > v_max: velocity[j] = v_max
+              if velocity[j] < -v_max: velocity[j] = -v_max
               
+              #Update position:
               new_position[j] = swarm[k][j] + velocity[j]
+
               # Validate position:
               if new_position[j] > max_: 
-                new_position[j] = max_
+                new_position[j] = max_ 
                 velocity[j] = 0 # check to see if this is correct
 
               if new_position[j] < min_:
@@ -195,44 +198,48 @@ def sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_ma
           swarm[k] = new_position
           velocities[k] = velocity
           fitness = objective_function(swarm[k])
-          
-          # if I = 0 particle is following gradient information
-          # if I = 1 particle is following global best information
-          #differently from other pso version, in sapso a particle can either follow one or another direction (in addition to alway following )
-          
-          #Check to see if we are improving fitness through iterations:
-          if I[k] == 0:
-              if abs(fitness - fitness_list[k]) <= stop_criterion:
-                  counter[k] += 1
-                  #If SAPSO can not improve fitness in 'c_max' iterations: then 'I' is 1 (particle will go onto the best global instead of gradient information)
-                  if counter[k] == c_max:
-                      I[k] = 1
-                      counter[k] = 0
-              else:
-                  counter[k] = 0
-          
-          else:
-              if np.sqrt(np.sum((swarm[k] - best_global_position)**2)) < stop_criterion:
-                  I[k] = 0
-          
-          fitness_list[k] = fitness
-          
-          #Verify if it's a new global best:
+
+           #Verify if it's a new global best:
           if fitness < best_global_fitness:
               best_global_fitness = fitness
               best_global_position = new_position
+          
+          fitness_list[k] = fitness
+
+      # if I = 0 particle is following gradient information
+      # if I = 1 particle is following global best information
+      #differently from other pso version, in sapso a particle can either follow one or another direction (in addition to alway following )
       
-      #Recalculate diversity
+      #After moving all particles, update their I value:
+      for k in range(n):    
+          #Check to see if we are improving fitness through iterations:
+          if I[k] == 0:
+              if abs(fitness_list[k] - fitness_list[k-1]) <= epislon:
+                  #If SAPSO can not improve fitness in 'c_max' iterations: then 'I' is 1 (particle will go onto the best global instead of gradient information)
+                  if counter[k] >= c_max:
+                      I[k] = 1
+                      counter[k] = 0
+
+                  counter[k] += 1
+                  
+              else:
+                  counter[k] = 0
+          
+          if I[k] == 1:
+              if np.sqrt(np.sum((swarm[k] - best_global_position)**2)) < epsilon_2:
+                  I[k] = 0
+                  counter[k] = 0
+      
+      #Recalculate diversity:
       diversity, dir_ = calculate_diversity_and_dir(d_low, d_high, n, L, swarm, dir_)
+      #
       best_fitness_history.append(best_global_fitness)
+      #Stop criterion:
+      #if len(best_fitness_history) >2 and abs(best_fitness_history[-1]-best_fitness_history[-2]) <= stop_criterion: break
   #recreating the fig object for new and final plot:
   fig = plt.figure(figsize=(9,9))
   plt.title('Final Position of particles')
   plot_swarm(swarm)
-  final_positions = 'final_positions.png'
-  plt.savefig(final_positions)
-  return final_positions, best_fitness_history, best_global_position
-
-
-
-#######################################################
+  final_positions_plot = 'final_positions_plot.png'
+  plt.savefig(final_positions_plot)
+  return final_positions_plot, best_fitness_history, best_global_position
--- using_sapso.py
+++ using_sapso.py
@@ -1,35 +1,39 @@
 # -*- coding: utf-8 -*-
 from sapso import sapso
 from PIL import Image
+from pprint import pprint
 ## Parameters 
-n = 80                 # Particles
+n = 20                 # Particles
 m = 5000               # Iterations
 n_dimensions = 2       # Number of dimensions
-min_ = -5.             # Search space size
-max_ = min_ * -1       # Seach space size  
+min_ = -50.            # Search space size
+max_ = 50.             # Seach space size  
 
-min_inertia = .4       # Inertial weight
-max_inertia = .7       # Inertial weight
-c1 = 1.5               # Cognitive coefficient
-c2 = 1.5               # Social coefficient
+min_inertia = .9       # Inertial weight
+max_inertia = .4       # Inertial weight
+c1 = 2                 # Cognitive coefficient
+c2 = 2                 # Social coefficient
 c_max = 3              # Max consecutive evaluations before finish optmizing
-stop_criterion = 1e-10 # If the algorithm cannot improve fitness it must stop
+epsilon = 1e-2         # If the algorithm cannot improve fitness it must stop
 d_low = 1e-1           # Lower threshold for diversity control
-d_high = .25          # Upper threshold for diversity control
+d_high = .25           # Upper threshold for diversity control
 
-f_name = 'ackley'   # Select your optmization
+f_name = 'sphere'      # Select your optmization
 
+stop_criterion = 1e-10
 
 #Using the optmizer:
-final_positions, best_fitness_history, best_global_position = sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_max, d_low, d_high, stop_criterion, f_name)
+final_positions_plot, best_fitness_history, best_global_position = sapso(n, m, n_dimensions, min_, max_, min_inertia, max_inertia, c1, c2, c_max, d_low, d_high, stop_criterion, f_name, stop_criterion)
 global_minimum = [0,0]
 
 print('Best fitness found was {} and actual global minimum is{}'.format(best_global_position,global_minimum))
+print("Fitness history was:")
+pprint(best_fitness_history)
 print('Particles\' last positions were: ')
 #...
 
-img = Image.open(final_positions)
+img = Image.open(final_positions_plot)
 img.show()
 #Destroy plot after execution:
 from os import remove
-remove(final_positions)
+remove(final_positions_plot)
