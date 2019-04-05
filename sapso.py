43c6a3495117031166bee5699e7a3ec83553f938
Marcelo <jmfda00@gmail.com>
2019-04-05 16:39:23 -0300

    Revert "Add gradient validation for performance improvement"
    This reverts commit 5cdf396b7c70391c3f4b9a5cf2e2bf6e1d85e0e4.

--- sapso.py
+++ sapso.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class TestFunctions():
-  
   def rosenbrock(self,x):
     """The Rosenbrock function"""
     return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
