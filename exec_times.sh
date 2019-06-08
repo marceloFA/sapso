3d9bb0e7511f221eaa8340bceccccb531825a17b
Marcelo Almeida <jmfda00@gmail.com>
2019-06-08 02:39:42 +0000

    update exec times

--- exec_times.sh
+++ exec_times.sh
@@ -1,8 +1,8 @@
 for function in {0,1,2,3,4,5,6}; do
-    for n_swarms in {5,10,15}; do
-        for n_dims in {2,4,8,10,20,90,100,150}; do
+    for n_swarms in {1,2,5,10,15}; do
+        for n_dims in {2,4,8,10,20,50,90,100,150}; do
             for n_execs in {1..20}; do
-                python exec_times.py ${n_dims} ${n_swarms} ${function} >> exec_times.txt; 
+                python3.7 exec_times.py ${n_dims} ${n_swarms} ${function} >> exec_times.txt; 
                 echo "iteration "${n_execs}" ended";
             done
             echo "Finished 20 execs for "${n_dims}" dims | function="${function};
